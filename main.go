653e84ff2d4f8591aea9f7f348922233b718dc6f
isongjosiah <isongjosiah@gmail.com>
2020-10-29 15:59:41 +0100

    Refactor main

--- main.go
+++ main.go
@@ -3,13 +3,9 @@ package main
 import (
 	"github.com/isongjosiah/lernen-api/api"
 	"log"
-	"net/http"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
 	//	start the api server
 	a := &api.API{}
 
