fe5761e491700d7127f40b2ce583b9689ee7a286
isongjosiah <isongjosiah@gmail.com>
2020-11-05 13:38:29 +0100

    Build test

--- api/user_handlers.go
+++ api/user_handlers.go
@@ -1,10 +1,18 @@
 package api
 
 import (
+	"fmt"
 	"github.com/go-chi/chi"
 	"net/http"
 )
 
 func (a *API) UserRoutes(router *chi.Mux) http.Handler {
+	router.Method("GET", "/home", http.HandlerFunc(a.UserHome))
 	return router
+}
+
+func (a *API)UserHome(w http.ResponseWriter, r *http.Request) {
+	if _, err := fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path); err != nil {
+		return
+	}
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,7 @@ module github.com/isongjosiah/lernen-api
 go 1.15
 
 require (
-	github.com/auth0/go-jwt-middleware v0.0.0-20201030150249-d783b5c46b39
 	github.com/caarlos0/env v3.5.0+incompatible // indirect
-	github.com/codegangsta/negroni v1.0.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/jinzhu/gorm v1.9.16
