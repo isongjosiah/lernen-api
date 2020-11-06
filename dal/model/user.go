6df7eff6c38963a5808df34ad713d5cfd3e9d18a
isongjosiah <isongjosiah@gmail.com>
2020-11-06 20:27:30 +0100

    Fix gorm type errors

--- dal/model/user.go
+++ dal/model/user.go
@@ -4,11 +4,11 @@ import "github.com/jinzhu/gorm"
 
 type User struct {
 	gorm.Model
-	Firstname string `gorm:"type:char" json:"firstname"`
-	Lastname  string `gorm:"type:char" json:"lastname"`
-	Username  string `gorm:"type:char" json:"username"`
+	Firstname string `gorm:"type:varchar" json:"firstname"`
+	Lastname  string `gorm:"type:varchar" json:"lastname"`
+	Username  string `gorm:"type:varchar" json:"username"`
 	Email     string `json:"email"`
-	Password  string `gorm:"type:char" json:"password"`
+	Password  string `gorm:"type:varchar" json:"password"`
 	Student   bool
 	//Enrolled  []string `gorm:"type:[]text"` TODO(josiah): sql doesn't allow a slice of strings. figure out a way around that
 }
