a4780f3a0b5dcc9d2f911aa38f741c0b6cf6c0b4
isongjosiah <isongjosiah@gmail.com>
2020-12-02 10:56:02 +0100

    Refactor authentication workflow to fit UI specification

--- api/auth_handlers_test.go
+++ api/auth_handlers_test.go
@@ -1,18 +1,10 @@
 package api_test
 
 import (
-	"github.com/isongjosiah/lernen-api/api"
-	"github.com/isongjosiah/lernen-api/dal/model"
 	. "github.com/onsi/ginkgo"
 )
 
 var _ = Describe("AuthHandlers", func() {
 	Describe("When testing authentication endpoints", func() {
-
-		var (
-			a                   *api.API
-			loginDetails        model.LoginDetails
-			registrationDetails model.User
-		)
 	})
 })
